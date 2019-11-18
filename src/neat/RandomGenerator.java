5e5a53bc1b9f1523fbe827db0976326e4bc49f45
AbhijeetKale <abhinitinkale@gmail.com>
2019-11-18 17:50:55 +0530

    Random probability basec action fixed

--- src/neat/RandomGenerator.java
+++ src/neat/RandomGenerator.java
@@ -4,11 +4,25 @@ import java.util.Random;
 
 public class RandomGenerator {
 	/*Total should be == 100*/
+	private static int abs(int n)
+	{
+		if(n < 0)
+			n *= -1;
+		return n;
+	}
+	private static double abs(double n)
+	{
+		if(n < 0.0)
+			n *= -1;
+		return n;
+	}
+
 	public static Object probablityBasedAction(Object[] list, double[] probs)
 	{
-		int sum = 0;
+		double sum = 0;
 		Random r = new Random();
-		double number = r.nextDouble() % 100;
+		double number = abs(r.nextInt()) + abs(r.nextDouble());
+		number = number % 100;
 		for(int count = 0; count < probs.length; count++)
 		{
 			sum += probs[count];
@@ -18,5 +32,11 @@ public class RandomGenerator {
 			}
 		}
 		return -1;
+	}
+	public static Object getRandomAction(Object[] list)
+	{
+		Random r = new Random();
+		int i = abs(r.nextInt()) % list.length;
+		return list[i];
 	}
 }
